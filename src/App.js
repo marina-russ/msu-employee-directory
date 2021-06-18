e4dbae852819a1ffca3bffbbd5bc9ca36a93973b
Marina Russ <marina@cameystudios.com>
2021-06-18 13:37:23 -0400

    functionality update forgot to make separate git commits

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import React from "react";
+import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import EmployeeCard from "./components/EmployeeCard";
-import employees from "./employees.json";
+
+import "./App.css";
 
 function App() {
   return (
-    <Wrapper>
-      <Header>Employee Directory</Header>
-      <EmployeeCard
-        name={employees[0].name}
-        image={employees[0].image}
-        occupation={employees[0].occupation}
-        branchLocation={employees[0].branch}
-      />
-      <EmployeeCard
-        name={employees[1].name}
-        image={employees[1].image}
-        occupation={employees[1].occupation}
-        branchLocation={employees[1].branch}
-      />
-      <EmployeeCard
-        name={employees[2].name}
-        image={employees[2].image}
-        occupation={employees[2].occupation}
-        branchLocation={employees[2].branch}
-      />
-      <EmployeeCard
-        name={employees[3].name}
-        image={employees[3].image}
-        occupation={employees[3].occupation}
-        branchLocation={employees[3].branch}
-      />
-      <EmployeeCard
-        name={employees[4].name}
-        image={employees[4].image}
-        occupation={employees[4].occupation}
-        branchLocation={employees[4].branch}
-      />
-    </Wrapper>
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
   );
 }
 
--- src/components/Header/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Header(props) {
-  return <h1 className="header">{props.children}</h1>;
-}
-
-export default Header;
